41769c064c036fb5108e95c5ebab46f019387d00
Mohammed Aashik <aashikdevelop@users.noreply.github.com>
2023-08-30 20:05:54 +0530

    Hulu-Clone commit

--- components/Nav.js
+++ components/Nav.js
@@ -12,12 +12,12 @@ function Nav() {
 
   return (
     <nav className="relative">
-      <div className="flex items-center px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20  overflow-x-scroll  overflow-y-hidden scrollbar-hide ">
+      <div className="flex items-center px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20  overflow-x-scroll  overflow-y-hidden scrollbar-hide  ">
         {Object.entries(requests).map(([key, { param, title, url }]) => (
           <Link href={`/?genre=${param}`}>
             <h2
               key={key}
-              className={` cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 `}
+              className={` cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500 lg:first:ml-16`}
             >
               {title}
             </h2>
